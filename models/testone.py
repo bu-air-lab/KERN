b2d7b531f116c272631731abb9d108799b0d528d
Saeid <astrosaeed@gmail.com>
2019-12-09 21:21:41 +0000

    temp

--- models/testone.py
+++ models/testone.py
@@ -1,5 +1,7 @@
 import sys
 sys.path.insert(0,"..")
+sys.path.insert(0,"../data/input_images")
+
 from config import ModelConfig
 from dataloaders.visual_genome import VGDataLoader, VG
 import numpy as np
@@ -17,16 +19,20 @@ import matplotlib.pyplot as plt
 import numpy
 from torch.autograd import Variable
 import torchvision.transforms as transforms
+from dataloaders.input import custom
 
+from dataloaders.image_transforms import SquarePad, Grayscale, Brightness, Sharpness, Contrast, \
+    RandomOrder, Hue, random_crop
+import os
 
-
-def imshow(img):
+def imshow(img,filename):
     img = img / 2 + 0.5     # unnormalize
     #npimg = img.numpy()
     npimg = img
 
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
+    #plt.show()
+    plt.savefig(filename+'.png')
 
 
 conf = ModelConfig()
@@ -82,21 +88,50 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=1,
 dataiter = iter(testloader)
 images, labels = dataiter.next()
 '''
+optimistic_restore(detector, ckpt['state_dict'])
+detector.eval()
+'''
+for val_b, batch in enumerate(tqdm(val_loader)):
+        print ('Hi')
+        det_res = detector[batch]
+        boxes_i, objs_i, obj_scores_i, rels_i, pred_scores_i= det_res
+        print('boxes_i :')
+        print(boxes_i)
+        print('length of boxes_i')
+        print (len(boxes_i))
+        print('obj_i :')
+        print(objs_i)
+        print('length of obj_i')
+        print (len(objs_i))
+        print('obj_scores :')
+        print(obj_scores_i)
+        print('length of obj_scores_i')
+        print (len(obj_scores_i))
+        print('rels_ is :')
+        print(rels_i)
+        print('length of rels_i')
+        print (len(rels_i))
+        print('pred_scores is :')
+        print(pred_scores_i)
+        print('length of pred_scores_i')
+        print (len(pred_scores_i))
+        print (len(pred_scores_i[0]))
 
 
 
 
-#dataiter = iter(val_loader)
-#images= dataiter.next()[0]
+        dataiter = iter(val_loader)
+        images= dataiter.next()[0]
 #print (images[0].data.numpy()[0,:,:,:].shape)
-#imshow(images[0].data.numpy()[0,:,:,:])
+        imshow(images[0].data.numpy()[0,:,:,:],str(val_b))
+'''
 #print (dataiter.next()[0])
 #print (val_loader.dataset.size())
 #print (ckpt.keys())
 #model.load_state_dict(ckpt )
 #model.eval()
 #images = torch.from_numpy(numpy.array(images))
-optimistic_restore(detector, ckpt['state_dict'])
+#optimistic_restore(detector, ckpt['state_dict'])
 #images = Variable(torch.from_numpy(numpy.array(images)))
 #print (images.size())
 print (val[0].keys())
@@ -104,9 +139,72 @@ print (train[0].keys())
 #print (val[0]['gt_boxes'])
 #print (val[0]['gt_classes'])
 print (val[0]['index'])
-#detector(Variable(val[0]['img'].view(-1,3,592,592).cuda()),[val[0]['img_size']],0,gt_boxes=val[0]['gt_boxes'], gt_classes=val[0]['gt_classes']  , gt_rels=val[0]['gt_relations']  , train_anchor_inds=val[0]['index'])
+print ('batch')
+for val_b, batch in enumerate(tqdm(val_loader)):
+	print (dir(batch))
+	print (type(batch.im_sizes))
+	print (batch.im_sizes)
+	break	
+	#sys.exit(0)
+
+print (np.asarray(val[0]['img_size']))
+#corrected_size=[[[val[0]['img_size']val[0]['img_size']val[0]['img_size']]]]
+#print (detector(Variable(val[0]['img'].view(-1,3,592,592).cuda()),np.asarray([val[0]['img_size']]),0,gt_boxes=val[0]['gt_boxes'], gt_classes=val[0]['gt_classes']  , gt_rels=val[0]['gt_relations']  , train_anchor_inds=val[0]['index']))
+
+#print (detector(Variable(val[0]['img'].view(-1,3,592,592).cuda()),np.asarray([val[0]['img_size']]),0,gt_boxes=np.zeros([1,4]), gt_classes=np.zeros([1,3])  , gt_rels=np.zeros([1,3]) , train_anchor_inds=0))
+
+#print ('testing on valloader: ')
+#print (detector[batch])
 
 
+
+all_inputs = os.listdir('/home/saeid/KERN/data/input_images')
+result_path= "/home/saeid/KERN/results/"
+input_path= "/home/saeid/KERN/data/input_images/"
+
+for image in all_inputs:
+
+	output = detector(custom(input_path+image)[0].cuda(),custom(input_path+image)[1],0,gt_boxes=np.zeros([1,4]), gt_classes=np.zeros([1,3])  , gt_rels=np.zeros([1,3]) , train_anchor_inds=0)
+
+	print ('dir output')
+	print (dir(output))
+	(boxes_i, objs_i, obj_scores_i, rels_i, pred_scores_i)= output
+
+
+	print ('boxes_i')
+	print (boxes_i)
+	print ('boxes_i.shape')
+	print (boxes_i.shape)
+	np.savetxt(result_path+image.split('.')[0]+"_boxes.csv",boxes_i,delimiter=",")
+	print (boxes_i.shape)
+	print ('objs_i')
+	print (objs_i)
+	np.savetxt(result_path+image.split('.')[0]+"_objs.csv",objs_i,delimiter=",")
+	print (len(objs_i))
+	print ('obj_scores_i')
+	print (obj_scores_i)
+	print (len(obj_scores_i))
+	print ('rels_i')
+	print (type(rels_i))
+	np.savetxt(result_path+image.split('.')[0]+"_rels.csv",rels_i,delimiter=",")
+	print (rels_i)
+	print (rels_i.shape)
+	print ('pred_scores_i')
+	print (pred_scores_i)
+	np.savetxt(result_path+image.split('.')[0]+"_pred_scores.csv",pred_scores_i,delimiter=",")
+	print (pred_scores_i.shape)
+
+	#input()
+
+'''
+print (type(Variable(val[0]['img'].view(-1,3,592,592))))
+print (val[0]['gt_boxes'])
+print (type(val[0]['gt_classes']))
+print (type(val[0]['gt_relations']) )
+print (val[0]['gt_relations'] )
+print (type(val[0]['index']))
+print (val[0]['index'])
+'''
 #if conf.mode == 'sgdet':
 #    det_ckpt = torch.load(conf.ckpt)['state_dict']
 #    detector.detector.bbox_fc.weight.data.copy_(det_ckpt['bbox_fc.weight'])
